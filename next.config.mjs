d960ca511d59602da430bbbeeed4ee1fc9a3b215
Marc-Olivier Fortin <info@marcolivierfortin.com>
2023-09-08 16:00:05 -0400

    Add domain redirect to prevent multiple hosts.

--- next.config.mjs
+++ next.config.mjs
@@ -10,4 +10,30 @@ export default {
     // Define the default locale for the application.
     defaultLocale: 'fr',
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'marcoeneurope.vercel.app',
+          },
+        ],
+        destination: 'https://marcoeneurope.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.marcoeneurope.com',
+          },
+        ],
+        destination: 'https://marcoeneurope.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
