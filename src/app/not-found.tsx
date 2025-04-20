33541c1cd135d52174370d31bf89776341766b02
Marc-Olivier Fortin <info@marcolivierfortin.com>
2025-04-19 23:45:15 -0400

    Add a not found error page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Render the not found error page.
+ *
+ * @return {React.JSX.Element}
+ */
+export default function NotFound(): React.JSX.Element {
+  return (
+    <div className={ 'page' }>
+      <main className="timeline">
+        <h2>404</h2>
+      </main>
+    </div>
+  );
+}
